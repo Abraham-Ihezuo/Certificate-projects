fe0fc5043fc1cb3ab10b4f5d413fbac1c0155431
Abraham <abrahamihezuo525@gmail.com>
2026-01-01 18:33:37 +0100

    setup react and vite for the random quote generator project for FCC

---
+++ random-quote-generator/src/App.jsx
@@ -0,0 +1,38 @@
+import { quotes } from "./data/quotes";
+import { useState } from "react";
+
+const App = () => {
+  const [quote, setQuote] = useState(quotes[0]);
+
+  const getNewQuote = () => {
+    const getRandom = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[getRandom]);
+  };
+
+  return (
+    <div id="quote-box" className="quote-box">
+      <div id="text" className="text">
+        {quote.text}
+      </div>
+      <div id="author" className="author">
+        {quote.author}
+      </div>
+
+      <div className="quote-btn-container">
+        <button id="new-quote" className="new-quote" onClick={getNewQuote}>
+          New Quote
+        </button>
+        <a
+          id="tweet-quote"
+          className="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text="${quote.text}" ${quote.author}`}
+          target="_blank"
+        >
+          Tweet Quote
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ random-quote-generator/src/data/quotes.js
@@ -0,0 +1,42 @@
+export const quotes = [
+  {
+    text: `“We cannot solve problems with the kind of thinking we employed when we came up with them.”`,
+    author: "-Albert Einstein",
+  },
+  {
+    text: `“Learn as if you will live forever, live like you will die tomorrow.”`,
+    author: "-Mahatma Gandhi",
+  },
+  {
+    text: `“Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.”`,
+    author: "-Mark Twain",
+  },
+  {
+    text: `“When you change your thoughts, remember to also change your world.”`,
+    author: "-Norman Vincent Peale",
+  },
+  {
+    text: `“It is only when we take chances that our lives improve. The initial and the most difficult risk we need to take is to become honest.” `,
+    author: "-Walter Anderson",
+  },
+  {
+    text: `“Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.” `,
+    author: "-Diane McLaren",
+  },
+  {
+    text: `"Every skilled person is to be believed with reference to his own art."`,
+    author: "-Legal maxim",
+  },
+  {
+    text: `"At bottom the world isn't a joke. We only joke about it to avoid an issue with someone, to let someone know that we know he's there with his questions; to disarm him by seeming to have heard and done justice to his side of the standing argument."`,
+    author: "-Robert Frost",
+  },
+  {
+    text: `"A friend is never known till a man has need."`,
+    author: "-Abraham Ihezuo",
+  },
+  {
+    text: `"A man loves his sweetheart the most, his wife the best, but his mother the longest."`,
+    author: "-Irish proverb",
+  },
+];
