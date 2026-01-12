8891cc2a2c3b5ef170840539c9d81c4275a6d0bb
Abraham <abrahamihezuo525@gmail.com>
2026-01-12 13:05:00 +0100

    Added drum machine

--- drum-machine/src/App.jsx
+++ drum-machine/src/App.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import { soundsStore } from "./data/sounds";
+import { useState, useEffect, useCallback } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [sounds, setSounds] = useState(soundsStore);
+  const [display, setDisplay] = useState("Sound Played");
+  const [volume, setVolume] = useState(0.3);
+
+  const playSound = useCallback(
+    (soundId, soundText) => {
+      const audio = document.getElementById(soundId);
+      if (audio) {
+        audio.currentTime = 0;
+        audio.volume = volume;
+        audio.play().catch((err) => console.log(err));
+        setDisplay(soundText);
+      }
+    },
+    [volume]
+  );
+
+  const handleVolume = (e) => {
+    setVolume(e.target.value);
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      const key = e.key.toUpperCase();
+      const sound = sounds.find((s) => s.id === key);
+      if (sound) {
+        playSound(sound.id, sound.text);
+        const button = document.getElementById(sound.text);
+        button.classList.add("active");
+        console.log(button);
+
+        setTimeout(() => {
+          button.classList.remove("active");
+        }, 100);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [sounds, playSound]);
+
+  return (
+    <div id="drum-machine" className="drum-machine">
+      <div className="drum-pad-container">
+        {sounds.map((sound) => {
+          return (
+            <button
+              key={sound.id}
+              id={sound.text}
+              onClick={() => playSound(sound.id, sound.text)}
+              className="drum-pad"
+            >
+              {sound.id}
+              <audio id={sound.id} src={sound.url} className="clip" />
+            </button>
+          );
+        })}
+      </div>
+
+      <div id="display" className="display">
+        {display}
+      </div>
+
+      <div className="volume-slide">
+        <label>Volume: {Math.round(volume * 100)}</label>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          className="range"
+          value={volume}
+          onChange={handleVolume}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ drum-machine/src/data/sounds.js
@@ -0,0 +1,47 @@
+export const soundsStore = [
+  {
+    id: "Q",
+    text: "Heater-1",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+  },
+  {
+    id: "W",
+    text: "Heater-2",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+  },
+  {
+    id: "E",
+    text: "Heater-3",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+  },
+  {
+    id: "A",
+    text: "Heater-4",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+  },
+  {
+    id: "S",
+    text: "Clap",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+  },
+  {
+    id: "D",
+    text: "Open-HH",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+  },
+  {
+    id: "Z",
+    text: "Kick-n'-Hat",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+  },
+  {
+    id: "X",
+    text: "Kick",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+  },
+  {
+    id: "C",
+    text: "Closed-HH",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3",
+  },
+];
